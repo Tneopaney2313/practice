e533f4633261ed86b3eebb076d7bb41ab6f62b3d
Tneopaney2313 <tneopaney2313@gmail.com>
2025-02-18 20:45:38 -0500

    3rd commit

--- src/Git2.java
+++ src/Git2.java
@@ -2,7 +2,9 @@ public class Git2 {
     public static void main(String[] args) {
 int a =3;
 int b= 3;
-        System.out.println( a+b);
+int c = 5;
+
+        System.out.println( a+b+c);
 
     }
 }
